f67e078a7732baeb434ec8eb98ba0707f02591c3
yann <yannlefranc330@gmail.com>
2023-04-14 19:16:27 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args){
+
+        String myGrade = "the witcher";
+        String Ciri = "ciri";
+
+        System.out.println("thewitcher");
+        System.out.println("ciri");
+
+
+    }
+}
